3e8270a7fcaffcaff368b3cbae5228bf1b735041
Richard Despas <richard.despas@gmail.com>
2021-12-06 00:36:20 -0700

    Day1 solutions for AoC 2021

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,47 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increments = 0;
+        var lastNumber = input.first().toInt()
+        for(i in 1 until input.size){
+            val number = input[i].toInt()
+            if(number > lastNumber){
+                increments++
+            }
+            lastNumber = number
+        }
+        return increments
     }
 
+    data class ThreeValueSlidingWindow(var first: Int, var second: Int, var third: Int)
+
     fun part2(input: List<String>): Int {
-        return input.size
+        var increments = 0
+        val listOfSlidingWindows = mutableListOf<ThreeValueSlidingWindow>()
+        for(line in input){
+            val number = line.toInt()
+            for(t in 0 until listOfSlidingWindows.size){
+                val window = listOfSlidingWindows[t]
+                if(window.second == -1){
+                    window.second = number
+                } else if (window.third == -1){
+                    window.third = number
+                    // this window is now complete, so we should be able to compare it to the one before, as long as its not the first
+                    if(t != 0){
+                        val lastWindow = listOfSlidingWindows[t-1]
+                        if(lastWindow.first + lastWindow.second + lastWindow.third < window.first + window.second + window.third){
+                            increments++
+                        }
+                    }
+                }
+            }
+            listOfSlidingWindows.add(ThreeValueSlidingWindow(number, -1, -1))
+            if(listOfSlidingWindows.size >= 2 && listOfSlidingWindows[1].third != -1){
+                // if the second window is complete, we're done with the first, and we
+                // can drop it to keep our space constant
+                listOfSlidingWindows.removeFirst()
+            }
+        }
+        return increments
     }
 
     // test if implementation meets criteria from the description, like:
